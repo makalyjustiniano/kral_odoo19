a0cc7f43b9da3033861306af5b12ceae9eb4eaa3
makalyjustiniano <makalyjustiniano@gmail.com>
2025-12-18 11:20:16 -0400

    dt_file_custom->fixing files

--- dt_file_custom/__manifest__.py
+++ dt_file_custom/__manifest__.py
@@ -2,11 +2,10 @@
 {
     'name': "Kral : Comprobantes de Movimiento Contable",
     'summary': """
-        Comprobantes Contable
+        Comprobantes Contables
     """,
     'description': """
-        - Comprobante de Ingreso
-        - Comprobante de Egreso
+        Comprobantes Contables
     """,
     'author': "Anthony Amutari Justiniano",
     'website': "https://kral.odoo.com.bo",
@@ -14,17 +13,7 @@
     'version': '19.0.1.0.0',
     'depends': ['account'],
     'data': [
-        #'report/report_compr_di.xml',
-        #'views/account_moveline_details_views.xml',
-        #report/report_ing_egre_.xml',
         'report/account_report_move.xml',
-        #'views/account_move_details_views.xml',
-
-        #'report/account_report_template.xml',
-        #'report/report_basic.xml',
-        #'report/report_basic_template.xml',
-        #'views/account_payment_details.xml',
-        #'views/account_move_report.xml',
     ],
     'demo': [
 
